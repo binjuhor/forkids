7e8cb1f79857e199094f027ceec249f24c395498
Binjuhor <binjuhor@gmail.com>
2018-04-15 05:38:36 +0700

    v1.0.0 + Embed Quill editor. + Final box editor on home. + Final css box editor.

---
+++ assets/js/kids.js
@@ -0,0 +1,46 @@
+(function($){
+    'use strict';
+
+    /**
+     * Tab for status post
+     */
+    $('.tab-button').on('click', function(e){
+        e.preventDefault();
+        $('.tab-button, .tab').removeClass('active');
+        $(this).addClass('active');
+        let tabShow = $(this).attr('href');
+        $(tabShow).addClass('active');
+    })
+
+    /**
+     * Editor for history post
+     * https://quilljs.com/docs/
+     * Version 
+     */
+    let toolbarOptions = [
+        ['bold', 'italic', 'underline', 'strike'],
+        ['blockquote', 'code-block'],
+
+        [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+
+        [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+
+        [{ 'color': [] }, { 'background': [] }],
+        [{ 'font': [] }],
+        [{ 'align': [] }],
+
+        ['clean']
+    ];
+
+    let options = {
+        // debug: 'info',
+        modules: {
+            toolbar: toolbarOptions
+        },
+        readOnly: false,
+        theme: 'snow'
+    };
+    let story = new Quill('.editor__quill', options); 
+    let status = new Quill('.editor__content'); 
+
+})(jQuery);
